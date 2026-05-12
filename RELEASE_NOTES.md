6ea7cce0568806739c589cb267735629b58ea7e2
superhac <superhac007@gmail.com>
2026-05-12 07:58:13 -0500

    Added option to disable Default Chrome options #2

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,10 +1,10 @@
 ## VPinFE Release Notes
 
 ### Summary
-This is the offical release of vpinfe v2.3.10
+This is the offical release of vpinfe v2.3.2
 
 ### What's New
-- Added option to disable Default Chrome options #2
+- Reverted "Added option to disable Default Chrome options #2"
 
 ### Fixes
 None
--- frontend/chromium_manager.py
+++ frontend/chromium_manager.py
@@ -84,17 +84,13 @@ def get_builtin_chromium_options(
     height = getattr(monitor, "height", "<height>")
     options = [
         f"--app={url}",
+        f"--window-name=vpinfe-{window_name}",
+        f"--window-position={x},{y}",
+        f"--window-size={width},{height}",
         f"--user-data-dir={user_data_dir}",
     ]
     if include_default_options:
-        options.extend(
-            [
-                f"--window-name=vpinfe-{window_name}",
-                f"--window-position={x},{y}",
-                f"--window-size={width},{height}",
-                *CHROMIUM_BASE_FLAGS,
-            ]
-        )
+        options.extend(CHROMIUM_BASE_FLAGS)
     if mute_audio:
         options.append("--mute-audio")
     return options
